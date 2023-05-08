39c7ad106e8d5c077501d268c98eb0d233c71bbf
marikadiblasio <marbla.albram@gmail.com>
2023-05-08 16:29:18 +0200

    wip

--- script.js
+++ script.js
@@ -6,20 +6,18 @@ createApp(
         data(){
             return {
                 title: 'PHP ToDo List JSON',
-                apiUrl: '../php-todo-list-json/server.php',
+                apiUrl: 'server.php',
             }
         },
         methods : {
             getData(){
                 axios.get(this.apiUrl).then((res)=>{
                     console.log(res.data);
-                    console.log(res.data[0]);
-                
-                })
+                });
             }
         },
         mounted(){
-             this.getData();
+            this.getData();
         }
     }
 ).mount('#app');
