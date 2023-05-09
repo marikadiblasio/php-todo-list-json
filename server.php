dbf95696eb6e13f05baf91d7e1176df6b9a76393
marikadiblasio <marbla.albram@gmail.com>
2023-05-09 07:33:05 +0200

    put changed array in sever.php

--- server.php
+++ server.php
@@ -14,7 +14,8 @@ if(isset($_POST['item'])){
 if(isset($_POST['completed']) && (isset($_POST['index']))) {
     $completed = $_POST['completed'];
     $index = $_POST['index'];
-    $phpItems[$index]['completed'] = $completed;
+    $phpItems[$index]['completed'] = filter_var($completed, FILTER_VALIDATE_BOOLEAN);
+    file_put_contents('./data.json', json_encode($phpItems));
 }
 header('Content-Type: application/json');
 echo json_encode($phpItems);
